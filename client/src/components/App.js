5e068292f81d7d0be8d394fe8be8319b125d6d8c
michaelmerrill <michael@michaelmerrill.io>
2018-08-24 13:57:56 -0600

    add session to localstorage

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,8 +3,17 @@ import './App.css';
 import Header from './Header';
 import Home from '../pages/Home';
 import Checkout from '../pages/Checkout';
+import uuid from 'uuid4';
 
 class App extends Component {
+  componentDidMount() {
+    const session = localStorage.getItem('formidableSession');
+    if (!session) {
+      const id = uuid();
+      localStorage.setItem('formidableSession', id);
+    }
+  }
+
   render() {
     return (
       <div className="App">
